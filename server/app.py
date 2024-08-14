791871cad3218ef257449b76b988b0ad0d173529
YUERONG HUANG <rowanne.h@hotmail.com>
2024-08-14 22:54:00 +1000

    complete lab

--- server/app.py
+++ server/app.py
@@ -87,12 +87,20 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        if not session['user_id']:
+            return {'error': 'Unauthorized'}, 401
+        articles = Article.query.filter_by(is_member_only=True).all()
+        articles_to_dict = [article.to_dict() for article in articles]
+        return articles_to_dict, 200
+    
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        if not session['user_id']:
+            return {'error': 'Unauthorized'}, 401
+        article = Article.query.filter_by(id=id, is_member_only=True).first()
+        return article.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
